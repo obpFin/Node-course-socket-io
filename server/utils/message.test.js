2b40211b8e1e627875d741aec8b9e9a7bbc5c8f8
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-04-02 21:56:57 +0300

    added geolocation support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,12 +1,24 @@
 var expect = require('chai').expect;
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
-describe('generate message', () => {
+describe('generateMessage', () => {
   it('should generate correct message object',() => {
     var from = "from";
     var text = "test";
     var message = generateMessage(from, text);
     expect(message.createdAt).to.be.a('number');
     expect(message).to.include({from,text});
+  });
+});
+
+describe('genereteLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'from';
+    var latitude = '123';
+    var longitude = '332';
+    var url = 'https://www.google.com/maps?q=123,332'
+    var locationMessage = generateLocationMessage(from, latitude, longitude);
+    expect(locationMessage.createdAt).to.be.a('number');
+    expect(locationMessage).to.include({from, url});
   });
 });
