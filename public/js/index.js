eb4d2a0e5362dbaa6bbabff694202148f0387bf8
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-04-07 20:01:07 +0300

    added mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -8,22 +8,25 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-  formattedTime = moment(message.createdAt).format('kk:mm');
-  console.log('newMessage', message);
-  var li  = jQuery('<li></li>');
-  li.text(`${message.from}: ${formattedTime}: ${message.text}`);
-
-  jQuery('#messages').append(li);
-})
+  var formattedTime = moment(message.createdAt).format('kk:mm');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
+});
 
 socket.on('newLocationMessage', function(message) {
-  formattedTime = moment(message.createdAt).format('kk:mm');
-  var li  = jQuery('<li></li>');
-  var a  = jQuery('<a target="_blank">My current location</a>');
-  li.text(`${message.from}: ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var formattedTime = moment(message.createdAt).format('kk:mm');
+  var template = jQuery('#location-template').html();
+  var html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function(e) {
